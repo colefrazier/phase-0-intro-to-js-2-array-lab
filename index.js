75dd49442b687537dc2a3fc141fb5c295fc6c573
colefrazier <cfrazier541@gmail.com>
2022-09-22 15:38:00 -0400

    commit tree 2e2ac4bff345177a9166c3facad7ed04840fd39c

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+console.log(cats)
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+cats.push("Ralp")
+console.log(cats)  
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop("Garfield")
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift("Milo")
+}
+
+function appendCat(name) {
+    return appendCat = [...cats, "Broom"]
+}
+function prependCat(name) {
+    return prependCat = ["Arnold", ...cats]
+}
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+function removeFirstCat() {
+    return cats.slice(1)
+}
